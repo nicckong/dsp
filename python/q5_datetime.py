0d31972f91f9d0b2025facd9423abacc5275bd50
nicckong <nicckong@gmail.com>
2017-08-23 16:19:40 +0800

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,19 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+diff = datetime.datetime.strptime(date_stop, '%m-%d-%Y').date() - datetime.datetime.strptime(date_start, '%m-%d-%Y').date()
+print ("{} days".format (diff.days))
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+diff = datetime.datetime.strptime(date_stop, '%m%d%Y').date() - datetime.datetime.strptime(date_start, '%m%d%Y').date()
+print ("{} days".format (diff.days))
+
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+diff = datetime.datetime.strptime(date_stop, '%d-%b-%Y').date() - datetime.datetime.strptime(date_start, '%d-%b-%Y').date()
+print ("{} days".format (diff.days))
